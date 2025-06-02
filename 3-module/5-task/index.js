559a17bad8fba144076f9c903362458102280cff
Михаил Гилодо <mgilodo@gmail.com>
2025-06-02 18:17:29 +0300

    Tasks from module 3 solved

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,15 @@
 function getMinMax(str) {
-  // ваш код...
+  const numbers = str
+    .split(' ')
+    .map(el => Number(el))
+    .filter(el => !isNaN(el));
+
+  if (numbers.length === 0) {
+    return { min: null, max: null };
+  }
+
+  return {
+    min: Math.min(...numbers),
+    max: Math.max(...numbers)
+  };
 }
